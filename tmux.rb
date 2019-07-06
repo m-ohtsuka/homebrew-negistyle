0234df9efd8e6baeb840209fd2882ee4166c9149
Masahito Ohtsuka <masahito@ootsuka.jp>
2019-07-06 13:53:15 +0900

    Removed the 'bottle'

--- tmux.rb
+++ tmux.rb
@@ -4,12 +4,6 @@ class Tmux < Formula
   url "https://github.com/tmux/tmux/releases/download/2.9a/tmux-2.9a.tar.gz"
   sha256 "839d167a4517a6bffa6b6074e89a9a8630547b2dea2086f1fad15af12ab23b25"
 
-  bottle do
-    cellar :any
-    sha256 "39e422362b56750f805e9725c498fd5653987a26e1b892d5f0d40820616be69d" => :mojave
-    sha256 "5c7148b6beb43995a0f53504f9a3f0500420e5ad11e17f34ce60401f42b833df" => :high_sierra
-    sha256 "b59dc70fea0e69398d757e5212d5d21642ee552518af5870f789312465c88500" => :sierra
-  end
   head do
     url "https://github.com/tmux/tmux.git"
 
