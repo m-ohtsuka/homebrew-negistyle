33c4ed2ad5c7a8c498d122ce2d6dd69d67155afa
Masahito Ohtsuka <masahito@ootsuka.jp>
2020-12-25 00:19:58 +0900

    tmux 3.1c_1

--- tmux.rb
+++ tmux.rb
@@ -3,6 +3,7 @@ class Tmux < Formula
   homepage "https://tmux.github.io/"
   url "https://github.com/tmux/tmux/releases/download/3.1c/tmux-3.1c.tar.gz"
   sha256 "918f7220447bef33a1902d4faff05317afd9db4ae1c9971bef5c787ac6c88386"
+  revision 1
 
   head do
     url "https://github.com/tmux/tmux.git"
