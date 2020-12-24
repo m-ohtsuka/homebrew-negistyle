c0967519463f8d5a4984e9959542624818380a0c
Masahito Ohtsuka <masahito@ootsuka.jp>
2020-12-25 00:36:02 +0900

    tmux 3.1c_1

--- tmux.rb
+++ tmux.rb
@@ -3,7 +3,14 @@ class Tmux < Formula
   homepage "https://tmux.github.io/"
   url "https://github.com/tmux/tmux/releases/download/3.1c/tmux-3.1c.tar.gz"
   sha256 "918f7220447bef33a1902d4faff05317afd9db4ae1c9971bef5c787ac6c88386"
+  license "ISC"
   revision 1
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+[a-z]?)["' >]}i)
+  end
 
   head do
     url "https://github.com/tmux/tmux.git"
@@ -13,30 +20,34 @@ class Tmux < Formula
     depends_on "libtool" => :build
   end
 
+  depends_on "pkg-config" => :build
+  depends_on "libevent"
+  depends_on "ncurses"
+
   patch do
     url "https://raw.githubusercontent.com/z80oolong/tmux-eaw-fix/master/tmux-3.1c-fix.diff"
     sha256 "2c2377b223c0841a3195effe83663f3be41d20b37de2b61eb8051f308125cd8d"
   end
-
-  depends_on "pkg-config" => :build
-  depends_on "libevent"
-  depends_on "ncurses"
-  depends_on "utf8proc"
+  
+  # Old versions of macOS libc disagree with utf8proc character widths.
+  # https://github.com/tmux/tmux/issues/2223
+  depends_on "utf8proc" if MacOS.version >= :high_sierra
 
   resource "completion" do
-    url "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/homebrew_1.0.0/completions/tmux"
-    sha256 "05e79fc1ecb27637dc9d6a52c315b8f207cf010cdcee9928805525076c9020ae"
+    url "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/f5d53239f7658f8e8fbaf02535cc369009c436d6/completions/tmux"
+    sha256 "b5f7bbd78f9790026bbff16fc6e3fe4070d067f58f943e156bd1a8c3c99f6a6f"
   end
 
   def install
     system "sh", "autogen.sh" if build.head?
 
     args = %W[
-      --enable-utf8proc
       --disable-dependency-tracking
       --prefix=#{prefix}
       --sysconfdir=#{etc}
     ]
+
+    args << "--enable-utf8proc" if MacOS.version >= :high_sierra
 
     ENV.append "LDFLAGS", "-lresolv"
     system "./configure", *args
